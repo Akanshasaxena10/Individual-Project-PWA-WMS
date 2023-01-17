112da42644d9a1ab91e78bedd0b2c8edd3292f5c
akansha <akanshasaxena10@gmail.com>
2023-01-17 14:43:42 +0100

    added new data file , created new branch function

--- cli/query.py
+++ cli/query.py
@@ -98,7 +98,124 @@ def option_menu():
     #     print('***********************************************') 
    
  ##'''I need to refractor this block of code as the stock and its type has change so the logic did.Refractor the code for doing that first understand the type of dictionary.
+# from data import stock
+# import datetime
 
+
+# print('1: List all items')
+# print('2: search and replace order')
+# print('3: browse y category')
+# print('4: Quit')
+
+# user_input = input("Choose between 1 -4. ")
+# # {"state": "Almost new", "category": "Keyboard", "warehouse": 1, 
+# #  "date_of_stock": "2020-05-06 08:28:12"}
+# while True:
+#     if user_input == '1':
+#         warehouse1 = [ f"{item['state']} {item['category']}" for item in stock if item.get('warehouse') == 1]
+#         warehouse2 = [ f"{item['state']} {item['category']}" for item in stock if item.get('warehouse') == 2]
+
+#         for item in set(warehouse1) | set(warehouse2):
+#             print('')
+#             print('-', item)
+#         print(f"Total items in warehouse1: {len(warehouse1)}")
+#         print(f"Total items in warehouse2: {len(warehouse2)}")
+#         print('')
+#         print('Thank you for your visit.')
+#         break
+# # {"state": "Almost new", "category": "Keyboard", "warehouse": 1, 
+# #  "date_of_stock": "2020-05-06 08:28:12"}
+#     if user_input == '2':
+#         item_name = input('What is the name of the item? ')
+#         wh1 = [item['date_of_stock'] for item in stock if item.get('warehouse') == 1 and f"{item['state']} {item['category']}".lower() == item_name.lower()]
+#         wh2 = [item['date_of_stock'] for item in stock if item.get('warehouse') == 2 and f"{item['state']} {item['category']}".lower() == item_name.lower()]
+#     if len(wh1 + wh2) == 0:
+#         print("Sorry, we do not have that item in stock.")
+#     else:
+#         today = datetime.datetime.now()
+#         for item in wh1:
+#             print(f"- Warehouse1 (in stock {(today - datetime.datetime.strptime(item, '%Y-%m-%d %H:%M:%S')).days} days)")
+#         for item in wh2:
+#             print(f"- Warehouse2 (in stock {(today - datetime.datetime.strptime(item, '%Y-%m-%d %H:%M:%S')).days} days)")
+
+#         break
+from data import stock
+import datetime
+import collections
+from itertools import zip_longest
+
+
+print('1: List all items')
+print('2: search and replace order')
+print('3: browse y category')
+print('4: Quit')
+
+user_input = input("Choose between 1 -4. ")
+# {"state": "Almost new", "category": "Keyboard", "warehouse": 1, 
+#  "date_of_stock": "2020-05-06 08:28:12"}
+while True:
+    if user_input == '1':
+        warehouse1 = [ f"{item['state']} {item['category']}" for item in stock if item.get('warehouse') == 1]
+        warehouse2 = [ f"{item['state']} {item['category']}" for item in stock if item.get('warehouse') == 2]
+
+        for item in set(warehouse1) | set(warehouse2):
+            print('')
+            print('-', item)
+        print(f"Total items in warehouse1: {len(warehouse1)}")
+        print(f"Total items in warehouse2: {len(warehouse2)}")
+        print('')
+        print('Thank you for your visit.')
+        break
+# {"state": "Almost new", "category": "Keyboard", "warehouse": 1, 
+#  "date_of_stock": "2020-05-06 08:28:12"}
+    if user_input == '2':
+        item_name = input('What is the name of the item? ')
+        wh1 = [item['date_of_stock'] for item in stock if item.get('warehouse') == 1 and f"{item['state']} {item['category']}".lower() == item_name.lower()]
+        wh2 = [item['date_of_stock'] for item in stock if item.get('warehouse') == 2 and f"{item['state']} {item['category']}".lower() == item_name.lower()]
+        if len(wh1 + wh2) == 0:
+            print("Amount available: 0")
+            print('Location: Not in Stock')
+        else:
+            today = datetime.datetime.now()
+            for item in wh1:
+                print(f"- Warehouse1 (in stock {(today - datetime.datetime.strptime(item, '%Y-%m-%d %H:%M:%S')).days} days)")
+            for item in wh2:
+                print(f"- Warehouse2 (in stock {(today - datetime.datetime.strptime(item, '%Y-%m-%d %H:%M:%S')).days} days)")
+            
+            if len(wh1) > 0 or len(wh2) > 0:
+                wh_d = {len(wh1): 'Warehouse 1', len(wh2): 'Warehouse 2'}
+                maximum = max(wh_d.keys())
+                print(f"Maximum availability: {maximum} in {wh_d[maximum]}")
+            break
+# {"state": "Almost new", "category": "Keyboard", "warehouse": 1, 
+#  "date_of_stock": "2020-05-06 08:28:12"}
+    if user_input == '3':
+        categories = [item['category'] for item in stock ]
+        categories_ct = collections.Counter(categories)
+        cat_d = {id: f"{category} ({categories_ct[category]})" for id, category in enumerate(categories_ct, start = 1)}
+        print()
+        for id, category in cat_d.items():
+            print(f"{id}. {category}")
+        get_id = int(input('Choose a category'))
+        print()
+        selected_cat = cat_d[get_id].split(" ")[0]
+        print(f'List of {selected_cat} available:')
+
+        wh1 = [ f"{item['state']} {item['category']}" 
+        for item in stock if item.get('warehouse') == 1 and item.get('category') == selected_cat]
+        wh2 = [ f"{item['state']} {item['category']}" for item in stock if item.get('warehouse') == 2 and item.get('category') == selected_cat]
+
+        print(len(set(wh1)))
+        print(len(set(wh2)))
+        print(list(zip(set(wh1), set(wh2))))
+        print(list(zip_longest(set(wh1), set(wh2), fillvalue=False)))
+
+        for item_wh1, item_wh2 in zip_longest(set(wh1), set(wh2), fillvalue=False):
+            if item_wh1: print(f"{item_wh1}, warehouse1")
+            if item_wh2: print(f"{item_wh2}, warehouse2")
+        print("Thank you for your visit")
+        break
+    
 def menu_1():
     if menu == 1:
         def filter_data(key,value):
